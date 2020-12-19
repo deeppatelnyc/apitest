7c0baf722abd8069d30062aeb61f4defc4602b51
deeppatelnyc <76241513+deeppatelnyc@users.noreply.github.com>
2020-12-19 04:19:06 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const userRouter = require("./api/user/user.router");
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use("/api/users", userRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("server running");
 });
